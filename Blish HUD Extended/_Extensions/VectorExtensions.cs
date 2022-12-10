76bf1845c63e04e3e784c66d54fd3d801cce3a9a
Andreas G <13819164+agaertner@users.noreply.github.com>
2022-12-10 12:59:08 +0100

    vector conversion

--- Blish HUD Extended/_Extensions/VectorExtensions.cs
+++ Blish HUD Extended/_Extensions/VectorExtensions.cs
@@ -6,16 +6,6 @@ namespace Blish_HUD.Extended
 {
     internal static class VectorExtensions
     {
-        public static Vector2 XY(this Vector3 vector)
-        {
-            return new(vector.X, vector.Y);
-        }
-
-        public static Vector2 XY(this System.Numerics.Vector3 vector)
-        {
-            return new(vector.X, vector.Y);
-        }
-
         public static Vector3 ToScreenSpace(this Vector3 position, Matrix view, Matrix projection)
         {
             int screenWidth = GameService.Graphics.SpriteScreen.Width;
@@ -38,16 +28,6 @@ namespace Blish_HUD.Extended
             return new Vector2((v.X / v.Z + 1f) / 2f * (float)GameService.Graphics.SpriteScreen.Width, (1f - v.Y / v.Z) / 2f * (float)GameService.Graphics.SpriteScreen.Height);
         }
 
-        public static Vector2 ToXna(this Coordinates2 coords)
-        {
-            return new Vector2((float)coords.X, (float)coords.Y);
-        }
-
-        public static Vector2 ToXna(this System.Numerics.Vector2 coords)
-        {
-            return new Vector2((float)coords.X, (float)coords.Y);
-        }
-
         public static float Distance(this Vector3 v1, Vector3 v2)
         {
             return (v1 - v2).Length();
@@ -56,6 +36,50 @@ namespace Blish_HUD.Extended
         public static double Angle(this Vector3 v, Vector3 u)
         {
             return Math.Acos(Vector3.Dot(v, u) / (v.Length() * u.Length()));
+        }
+
+        public static Vector2 XY(this Vector3 vector)
+        {
+            return new(vector.X, vector.Y);
+        }
+
+        #region Conversion
+        /// <summary>
+        /// Converts a <see cref="System.Numerics.Vector2"/> to a <see cref="Microsoft.Xna.Framework.Vector2"/>.
+        /// </summary>
+        public static Vector2 ToXna(this System.Numerics.Vector2 coords)
+        {
+            return new Vector2(coords.X, coords.Y);
+        }
+        /// <summary>
+        /// Converts a <see cref="Microsoft.Xna.Framework.Vector2"/> to a <see cref="System.Numerics.Vector2"/>.
+        /// </summary>
+        public static System.Numerics.Vector2 ToSystem(this Vector2 coords)
+        {
+            return new System.Numerics.Vector2(coords.X, coords.Y);
+        }
+        /// <summary>
+        /// Converts a <see cref="System.Numerics.Vector3"/> to a <see cref="Microsoft.Xna.Framework.Vector3"/>.
+        /// </summary>
+        public static Vector3 ToXna(this System.Numerics.Vector3 coords)
+        {
+            return new Vector3(coords.X, coords.Y, coords.Z);
+        }
+        /// <summary>
+        /// Converts a <see cref="Microsoft.Xna.Framework.Vector3"/> to a <see cref="System.Numerics.Vector3"/>.
+        /// </summary>
+        public static System.Numerics.Vector3 ToSystem(this Vector3 coords)
+        {
+            return new System.Numerics.Vector3(coords.X, coords.Y, coords.Z);
+        }
+        #endregion
+    }
+
+    public static class CoordinatesExtensions
+    {
+        public static Vector2 ToVector(this Coordinates2 coords)
+        {
+            return new Vector2((float)coords.X, (float)coords.Y);
         }
     }
 }
