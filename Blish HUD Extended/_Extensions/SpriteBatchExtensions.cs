35c19a4fae96f6d182848b3fb260ad1aad7b8e7b
Andreas G <13819164+agaertner@users.noreply.github.com>
2023-03-03 01:08:27 +0100

    fix rectangle

--- Blish HUD Extended/_Extensions/SpriteBatchExtensions.cs
+++ Blish HUD Extended/_Extensions/SpriteBatchExtensions.cs
@@ -8,8 +8,8 @@ namespace Blish_HUD.Extended
     {
         public static void DrawRectangleOnCtrl(this SpriteBatch spriteBatch, Control ctrl, Rectangle bounds, int lineWidth, Color color)
         {
-            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X - lineWidth, bounds.Y - lineWidth, bounds.Width + lineWidth, lineWidth), color);
-            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X - lineWidth, bounds.Y, lineWidth, bounds.Height + lineWidth), color);
+            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y - lineWidth, bounds.Width + lineWidth, lineWidth), color);
+            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y, lineWidth, bounds.Height + lineWidth), color);
             spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y + bounds.Height, bounds.Width + lineWidth, lineWidth), color);
             spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X + bounds.Width, bounds.Y - lineWidth, lineWidth, bounds.Height + lineWidth), color);
         }
