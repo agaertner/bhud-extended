764286b9e01b92b777fe4631148f7750b7c8d64f
Andreas G <13819164+agaertner@users.noreply.github.com>
2023-04-11 02:53:17 +0200

    chore: removed Flurl.Http and social views

--- Blish HUD Extended/_Utils/TaskUtil.cs
+++ Blish HUD Extended/_Utils/TaskUtil.cs
@@ -1,87 +1,11 @@
-﻿using Flurl.Http;
-using Gw2Sharp.WebApi.Exceptions;
-using Newtonsoft.Json;
+﻿using Gw2Sharp.WebApi.Exceptions;
 using System;
-using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Blish_HUD.Extended
 {
     public class TaskUtil
     {
-        private static Logger Logger = Logger.GetLogger<TaskUtil>();
-        public static void CallActionWithTimeout(Action action, Action error, int timeout)
-        {
-            var cancelToken = new CancellationTokenSource();
-            var token = cancelToken.Token;
-            var task = Task.Run(delegate
-            {
-                try
-                {
-                    var thread = Thread.CurrentThread;
-                    using (token.Register(thread.Abort))
-                    {
-                        action();
-                    }
-                    return null;
-                }
-                catch (Exception ex)
-                {
-                    return ex;
-                }
-            }, token);
-            var index = Task.WaitAny(task, Task.Delay(timeout));
-            if (index != 0)
-            {
-                cancelToken.Cancel();
-                error();
-            }
-            else if (task.Result != null)
-            {
-                Logger.Error(task.Result.Message, task.Result);
-            }
-        }
-
-        public static bool TryParseJson<T>(string json, out T result)
-        {
-            bool success = true;
-            var settings = new JsonSerializerSettings
-            {
-                Error = (_, args) => { success = false; args.ErrorContext.Handled = true; },
-                MissingMemberHandling = MissingMemberHandling.Error
-            };
-            result = JsonConvert.DeserializeObject<T>(json, settings);
-            return success;
-        }
-
-        public static async Task<(bool, T)> GetJsonResponse<T>(string request, int timeOutSeconds = 10)
-        {
-            try
-            {
-                var rawJson = await request.AllowHttpStatus(HttpStatusCode.NotFound).AllowHttpStatus("200").WithTimeout(timeOutSeconds).GetStringAsync();
-                return (TryParseJson<T>(rawJson, out var result), result);
-            }
-            catch (FlurlHttpTimeoutException ex)
-            {
-                Logger.Warn(ex, $"Request '{request}' timed out.");
-            }
-            catch (FlurlHttpException ex)
-            {
-                Logger.Warn(ex, $"Request '{request}' was not successful.");
-            }
-            catch (JsonReaderException ex)
-            {
-                Logger.Warn(ex, $"Failed to deserialize requested content from \"{request}\"\n{ex.StackTrace}");
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex, $"Unexpected error while requesting '{request}'.");
-            }
-
-            return (false, default);
-        }
-
         /// <summary>
         /// Retries the given awaitable <see cref="Task{T}"/> function a given amount of times each after a set delay (default: 30s).
         /// </summary>
@@ -89,9 +13,12 @@ namespace Blish_HUD.Extended
         /// <param name="func">The awaitable function to retry.</param>
         /// <param name="retries">Amount of retries before an exception is logged.</param>
         /// <param name="delayMs">A delay in milliseconds to wait before trying again.</param>
+        /// <param name="logger">An optional logger that is used for exception messages.</param>
         /// <returns><see cref="Task{T}"/> if successful; otherwise <see cref="Task"/>&lt;<see langword="default"/>&gt;.</returns>
-        public static async Task<T> RetryAsync<T>(Func<Task<T>> func, int retries = 2, int delayMs = 30000)
+        public static async Task<T> RetryAsync<T>(Func<Task<T>> func, int retries = 2, int delayMs = 30000, Logger logger = null)
         {
+            logger ??= Logger.GetLogger<TaskUtil>();
+
             try
             {
                 return await func();
@@ -101,27 +28,27 @@ namespace Blish_HUD.Extended
                 // Do not retry if requested resource does not exist or access is denied.
                 if (e is NotFoundException or BadRequestException or AuthorizationRequiredException)
                 {
-                    Logger.Trace(e, e.Message);
+                    logger.Trace(e, e.Message);
                     return default;
                 }
 
                 if (retries > 0)
                 {
-                    Logger.Warn(e, $"Failed to pull data from the GW2 API. Retrying in {delayMs / 1000} second(s) (remaining retries: {retries}).");
+                    logger.Warn(e, $"Failed to pull data from the GW2 API. Retrying in {delayMs / 1000} second(s) (remaining retries: {retries}).");
                     await Task.Delay(delayMs);
-                    return await RetryAsync(func, retries - 1, delayMs);
+                    return await RetryAsync(func, retries - 1, delayMs, logger);
                 }
 
                 switch (e)
                 {
                     case TooManyRequestsException:
-                        Logger.Warn(e, "After multiple attempts no data could be loaded due to being rate limited by the API.");
+                        logger.Warn(e, "After multiple attempts no data could be loaded due to being rate limited by the API.");
                         break;
                     case RequestException or RequestException<string>:
-                        Logger.Trace(e, e.Message);
+                        logger.Trace(e, e.Message);
                         break;
                     default:
-                        Logger.Error(e, e.Message);
+                        logger.Error(e, e.Message);
                         break;
                 }
 
@@ -134,9 +61,12 @@ namespace Blish_HUD.Extended
         /// </summary>
         /// <typeparam name="T">Some type returned by the <see cref="Task"/> function.</typeparam>
         /// <param name="func">The awaitable function to try.</param>
+        /// <param name="logger">An optional logger that is used for exception messages.</param>
         /// <returns><see cref="Task{T}"/> if successful; otherwise <see cref="Task"/>&lt;<see langword="default"/>&gt;.</returns>
-        public static async Task<T> TryAsync<T>(Func<Task<T>> func)
+        public static async Task<T> TryAsync<T>(Func<Task<T>> func, Logger logger = null)
         {
+            logger ??= Logger.GetLogger<TaskUtil>();
+
             try
             {
                 return await func();
@@ -146,16 +76,16 @@ namespace Blish_HUD.Extended
                 switch (e)
                 {
                     case NotFoundException or BadRequestException or AuthorizationRequiredException: // Resource does not exist or access is denied.
-                        Logger.Trace(e, e.Message);
+                        logger.Trace(e, e.Message);
                         break;
                     case TooManyRequestsException:
-                        Logger.Warn(e, "No data could be loaded due to being rate limited by the API.");
+                        logger.Warn(e, "No data could be loaded due to being rate limited by the API.");
                         break;
                     case RequestException or RequestException<string>:
-                        Logger.Trace(e, e.Message);
+                        logger.Trace(e, e.Message);
                         break;
                     default:
-                        Logger.Error(e, e.Message);
+                        logger.Error(e, e.Message);
                         break;
                 }
                 return default;
