c294c32840182d149392a084eb03d7ee178a07fa
Andreas G <13819164+agaertner@users.noreply.github.com>
2023-08-08 11:12:20 +0200

    fix: draw rectangle in bounds

--- Blish HUD Extended/_Extensions/SpriteBatchExtensions.cs
+++ Blish HUD Extended/_Extensions/SpriteBatchExtensions.cs
@@ -8,10 +8,17 @@ namespace Blish_HUD.Extended
     {
         public static void DrawRectangleOnCtrl(this SpriteBatch spriteBatch, Control ctrl, Rectangle bounds, int lineWidth, Color color)
         {
-            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y - lineWidth, bounds.Width + lineWidth, lineWidth), color);
-            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y, lineWidth, bounds.Height + lineWidth), color);
-            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y + bounds.Height, bounds.Width + lineWidth, lineWidth), color);
-            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X + bounds.Width, bounds.Y - lineWidth, lineWidth, bounds.Height + lineWidth), color);
+            // Top-Left to Top-Right
+            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y, bounds.Width, lineWidth), color);
+
+            // Top-Left to Bottom-Left
+            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y, lineWidth, bounds.Height), color);
+
+            // Bottom-Left to Bottom-Right
+            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X, bounds.Y + bounds.Height - lineWidth, bounds.Width, lineWidth), color);
+
+            // Top-Right to Bottom-Right
+            spriteBatch.DrawOnCtrl(ctrl, ContentService.Textures.Pixel, new Rectangle(bounds.X + bounds.Width - lineWidth, bounds.Y, lineWidth, bounds.Height), color);
         }
 
         public static void DrawRectangleOnCtrl(this SpriteBatch spriteBatch, Control ctrl, Rectangle bounds, int lineWidth)
