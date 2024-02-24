c578dbe1e5e575adf6bc19c6ac636ac847e52398
Andreas G <13819164+agaertner@users.noreply.github.com>
2024-02-24 20:47:43 +0100

    feat: add custom runtime font

--- Blish HUD Extended/_Extensions/ContentsManagerExtensions.cs
+++ Blish HUD Extended/_Extensions/ContentsManagerExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using Blish_HUD.Modules.Managers;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.BitmapFonts;
+using SpriteFontPlus;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -48,6 +51,50 @@ namespace Blish_HUD.Extended
             {
                 Logger.Warn(e, e.Message);
             }
+        }
+
+        /// <summary>
+        /// Loads a <see cref="SpriteFont"/> from a TrueTypeFont (*.ttf) file.
+        /// </summary>
+        /// <param name="manager">Module's <see cref="ContentsManager"/>.</param>
+        /// <param name="fontPath">The path to the TTF font file.</param>
+        /// <param name="fontSize">Size of the font.</param>
+        /// <param name="textureSize">Size of the <see cref="SpriteFont.Texture"/>.<br/>A greater <c>fontSize</c> results in bigger glyphs which may require more texture space.</param>
+        public static SpriteFont GetSpriteFont(this ContentsManager manager, string fontPath, int fontSize, int textureSize = 1392)
+        {
+            if (fontSize <= 0)
+            {
+                throw new ArgumentException("Font size must be greater than 0.", nameof(fontSize));
+            }
+
+            using var fontStream = manager.GetFileStream(fontPath);
+            var fontData = new byte[fontStream.Length];
+            var fontDataLength = fontStream.Read(fontData, 0, fontData.Length);
+
+            if (fontDataLength > 0)
+            {
+                using var ctx = GameService.Graphics.LendGraphicsDeviceContext();
+                var bakeResult = TtfFontBaker.Bake(fontData, fontSize, textureSize, textureSize, new[] {
+                    CharacterRange.BasicLatin,
+                    CharacterRange.Latin1Supplement,
+                    CharacterRange.LatinExtendedA
+                });
+                return bakeResult.CreateSpriteFont(ctx.GraphicsDevice);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Loads a <see cref="BitmapFont"/> from a TrueTypeFont (*.ttf) file.
+        /// </summary>
+        /// <param name="manager">Module's <see cref="ContentsManager"/>.</param>
+        /// <param name="fontPath">The path to the TTF font file.</param>
+        /// <param name="fontSize">Size of the font.</param>
+        /// <param name="lineHeight">Sets the line height. By default, <see cref="SpriteFont.LineSpacing"/> will be used.</param>
+        public static BitmapFontEx GetBitmapFont(this ContentsManager manager, string fontPath, int fontSize, int lineHeight = 0)
+        {
+            return manager.GetSpriteFont(fontPath, fontSize)?.ToBitmapFont(lineHeight);
         }
     }
 }
---
+++ Blish HUD Extended/_Utils/BitmapFontEx.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.BitmapFonts;
+using System;
+using System.Collections.Generic;
+
+namespace Blish_HUD.Extended
+{
+    public class BitmapFontEx : BitmapFont, IDisposable
+    {
+        private readonly Texture2D _texture;
+
+        public BitmapFontEx(string name, IEnumerable<BitmapFontRegion> regions, int lineHeight, Texture2D texture) : base(name, regions, lineHeight)
+        {
+            _texture = texture;
+        }
+
+        public BitmapFontEx(string name, IReadOnlyList<BitmapFontRegion> regions, int lineHeight) : base(name, regions, lineHeight)
+        {
+            _texture = regions[0].TextureRegion.Texture;
+        }
+
+        public void Dispose()
+        {
+            _texture?.Dispose();
+        }
+    }
+}
