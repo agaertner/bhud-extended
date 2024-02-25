a65c4881d25b9f5d59f722e8c9b6729ae48b872d
Andreas G <13819164+agaertner@users.noreply.github.com>
2024-02-25 23:37:48 +0100

    feat: add supported or default.

--- Blish HUD Extended/_Extensions/LocaleExtensions.cs
+++ Blish HUD Extended/_Extensions/LocaleExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Gw2Sharp.WebApi;
+using System.Linq;
 
 namespace Blish_HUD.Extended
 {
@@ -24,6 +25,26 @@ namespace Blish_HUD.Extended
                 Locale.Chinese => "zh",
                 _ => "en"
             };
+        }
+
+        /// <summary>
+        /// Checks if the given locale is supported and returns the default supported locale if not.
+        /// </summary>
+        /// <param name="locale">Locale to verify.</param>
+        /// <param name="supported">Custom list of supported languages. The first entry is the default fallback.</param>
+        /// <returns>Given locale or default fallback locale.</returns>
+        /// <remarks>English, Spanish, German, French are the default supported locales.</remarks>
+        public static Locale SupportedOrDefault(this Locale locale, params Locale[] supported) {
+            if (supported?.Any() ?? true) {
+                return locale switch
+                {
+                    Locale.Spanish => locale,
+                    Locale.German => locale,
+                    Locale.French => locale,
+                    _ => Locale.English
+                };
+            }
+            return supported.Contains(locale) ? locale : supported.FirstOrDefault();
         }
     }
 }
