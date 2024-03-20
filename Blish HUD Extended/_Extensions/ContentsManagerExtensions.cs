8fd235de0d35277b0eb1f901889c4a58219a8509
Andreas G <13819164+agaertner@users.noreply.github.com>
2024-03-20 17:09:07 +0100

    fix: missing fonts

--- Blish HUD Extended/_Extensions/ContentsManagerExtensions.cs
+++ Blish HUD Extended/_Extensions/ContentsManagerExtensions.cs
@@ -12,6 +12,25 @@ namespace Blish_HUD.Extended
     {
         private static Logger Logger = Logger.GetLogger<ContentsManager>();
 
+        internal static CharacterRange GeneralPunctuation = new('\u2000', '\u206F');
+        internal static CharacterRange Arrows = new('\u2190', '\u21FF');
+        internal static CharacterRange MathematicalOperators = new('\u2200', '\u22FF');
+        internal static CharacterRange BoxDrawing = new('\u2500', '\u2570');
+        internal static CharacterRange GeometricShapes = new('\u25A0', '\u25FF');
+        internal static CharacterRange MiscellaneousSymbols = new('\u2600', '\u26FF');
+
+        internal static readonly CharacterRange[] Gw2CharacterRange = {
+            CharacterRange.BasicLatin,
+            CharacterRange.Latin1Supplement,
+            CharacterRange.LatinExtendedA,
+            GeneralPunctuation,
+            Arrows,
+            MathematicalOperators,
+            BoxDrawing,
+            GeometricShapes,
+            MiscellaneousSymbols
+        };
+
         /// <summary>
         /// Extracts a file from the module archive.
         /// </summary>
@@ -74,11 +93,7 @@ namespace Blish_HUD.Extended
             if (fontDataLength > 0)
             {
                 using var ctx = GameService.Graphics.LendGraphicsDeviceContext();
-                var bakeResult = TtfFontBaker.Bake(fontData, fontSize, textureSize, textureSize, new[] {
-                    CharacterRange.BasicLatin,
-                    CharacterRange.Latin1Supplement,
-                    CharacterRange.LatinExtendedA
-                });
+                var bakeResult = TtfFontBaker.Bake(fontData, fontSize, textureSize, textureSize, Gw2CharacterRange);
                 return bakeResult.CreateSpriteFont(ctx.GraphicsDevice);
             }
 
