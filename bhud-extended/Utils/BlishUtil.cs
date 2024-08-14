426e4d8c25ea5387230f6bdd482f68f899a52c5c
Andreas G. <13819164+agaertner@users.noreply.github.com>
2024-08-14 22:04:56 +0200

    add getVersion

---
+++ bhud-extended/Utils/BlishUtil.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Blish_HUD.Extended
+{
+    public class BlishUtil {
+        /// <summary>
+        /// Gets the Blish HUD version.
+        /// </summary>
+        /// <returns>Blish HUD's assembly version.</returns>
+        public static string GetVersion() {
+            var version = typeof(BlishHud).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            var release = string.IsNullOrEmpty(version) ? string.Empty : $"Blish HUD v{version.Split('+').First()}";
+            return release;
+        }
+    }
+}
