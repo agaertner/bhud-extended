de0063b1811d18cdcf7404d7cb0f69cbbfeaa393
Andreas G. <13819164+agaertner@users.noreply.github.com>
2024-10-02 13:25:15 +0200

    feat: add mumble extensions

---
+++ bhud-extended/Extensions/BlishHudExtensions/Gw2MumbleServiceExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Blish_HUD.Gw2Mumble;
+using Microsoft.Xna.Framework;
+using System;
+// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
+
+namespace Blish_HUD.Extended
+{
+    public static class Gw2MumbleServiceExtensions {
+        public static Vector3 Position(this PlayerCharacter playerCharacter, bool swapYZ)
+        {
+            return swapYZ ? playerCharacter.Position.SwapYZ() : playerCharacter.Position;
+        }
+
+        public static Vector3 Position(this PlayerCamera playerCamera, bool swapYZ)
+        {
+            return swapYZ ? playerCamera.Position.SwapYZ() : playerCamera.Position;
+        }
+
+        public static Vector3 Forward(this PlayerCharacter playerCharacter, bool swapYZ)
+        {
+            return swapYZ ? playerCharacter.Forward.SwapYZ() : playerCharacter.Forward;
+        }
+
+        public static Vector3 Forward(this PlayerCamera playerCamera, bool swapYZ)
+        {
+            return swapYZ ? playerCamera.Forward.SwapYZ() : playerCamera.Forward;
+        }
+
+        private const int MAPWIDTH_MAX = 362;
+        private const int MAPHEIGHT_MAX = 338;
+        private const int MAPWIDTH_MIN = 170;
+        private const int MAPHEIGHT_MIN = 170;
+        private const int MAPOFFSET_MIN = 19;
+
+        private static int GetOffset(float curr, float max, float min, float val)
+        {
+            return (int)Math.Round((curr - min) / (max - min) * (val - MAPOFFSET_MIN) + MAPOFFSET_MIN, 0);
+        }
+
+        public static Rectangle CompassBounds(this UI ui)
+        {
+            int offsetWidth = GetOffset(ui.CompassSize.Width, MAPWIDTH_MAX, MAPWIDTH_MIN, 40);
+            int offsetHeight = GetOffset(ui.CompassSize.Height, MAPHEIGHT_MAX, MAPHEIGHT_MIN, 40);
+            int width = ui.CompassSize.Width + offsetWidth;
+            int height = ui.CompassSize.Height + offsetHeight;
+            int x = GameService.Graphics.SpriteScreen.ContentRegion.Width - width;
+            int y = 0;
+            if (!ui.IsCompassTopRight) {
+                y += GameService.Graphics.SpriteScreen.ContentRegion.Height - height - 40;
+            }
+            return new Rectangle(x, y, width, height);
+        }
+
+        private static Vector3 SwapYZ(this Vector3 vec)
+        {
+            return new Vector3(vec.X, vec.Z, vec.Y);
+        }
+    }
+}
---
+++ bhud-extended/Extensions/Gw2SharpExtensions/CoordinatesExtensions.cs
@@ -0,0 +1,81 @@
+﻿using Gw2Sharp.Models;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Rectangle = Gw2Sharp.WebApi.V2.Models.Rectangle;
+// ReSharper disable InconsistentNaming
+
+namespace Blish_HUD.Extended
+{
+    public static class CoordinatesExtensions
+    {
+        private const float INCH_TO_METER = 0.0254F;
+        
+        public static Coordinates3 SwapYZ(this Coordinates3 coords)
+        {
+            return new Coordinates3(coords.X, coords.Z, coords.Y);
+        }
+
+        public static Coordinates2 ToPlane(this Coordinates3 coords)
+        {
+            return new Coordinates2(coords.X, coords.Y);
+        }
+
+        public static Coordinates3 ToUnit(this Coordinates3 coords, CoordsUnit fromUnit, CoordsUnit toUnit)
+        {
+            if (fromUnit == CoordsUnit.Meters && toUnit == CoordsUnit.Inches)
+                return new Coordinates3(coords.X / INCH_TO_METER, coords.Y / INCH_TO_METER, coords.Z / INCH_TO_METER);
+            else if (fromUnit == CoordsUnit.Inches && toUnit == CoordsUnit.Meters)
+                return new Coordinates3(coords.X * INCH_TO_METER, coords.Y * INCH_TO_METER, coords.Z * INCH_TO_METER);
+            return coords;
+        }
+
+        public static Coordinates3 ToMapCoords(this Coordinates3 coords, CoordsUnit fromUnit)
+        {
+            coords = coords.ToUnit(fromUnit, CoordsUnit.GameWorld);
+            return new Coordinates3(coords.X, coords.Y, coords.Z);
+        }
+
+        public static Coordinates3 ToContinentCoords(this Coordinates3 coords, CoordsUnit fromUnit, Rectangle mapRectangle, Rectangle continentRectangle)
+        {
+            var mapCoords = coords.ToMapCoords(fromUnit);
+            double x = ((mapCoords.X - mapRectangle.TopLeft.X) / mapRectangle.Width * continentRectangle.Width) + continentRectangle.TopLeft.X;
+            double z = ((1 - ((mapCoords.Z - mapRectangle.BottomRight.Y) / mapRectangle.Height)) * continentRectangle.Height) + continentRectangle.TopRight.Y;
+            return new Coordinates3(x, mapCoords.Y, z);
+        }
+
+        public static Vector3 ToXnaVector3(this Coordinates3 coords)
+        {
+            return new Vector3((float)coords.X, (float)coords.Y, (float)coords.Z);
+        }
+
+        public static bool Inside(this Coordinates2 targetPoint, IReadOnlyList<Coordinates2> polygon)
+        {
+            if (polygon.Count < 3)
+            {
+                // Must have at least 3 vertices to be valid.
+                return false;
+            }
+            double x = targetPoint.X;
+            double y = targetPoint.Y;
+            bool isInside = false;
+            for (int i = 0, j = polygon.Count - 1; i < polygon.Count; j = i++)
+            {
+                if ((polygon[i].Y > y) != (polygon[j].Y > y) &&
+                    x < (polygon[j].X - polygon[i].X) * (y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X)
+                {
+                    isInside = !isInside;
+                }
+            }
+            return isInside;
+        }
+    }
+
+    public enum CoordsUnit
+    {
+        Inches,
+        GameWorld = Inches,
+
+        Meters,
+        Mumble = Meters
+    }
+}
